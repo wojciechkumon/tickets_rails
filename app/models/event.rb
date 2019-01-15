28dabfe2257c07271245163d61cf560b9f9c2d08
Wojciech Kumoń <wojtas626@gmail.com>
2019-01-15 21:49:18 +0100

    Event validators added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+
+  validates :artist, presence: true
+  validates :price_low, presence: true, numericality: true
+  validates :price_high, presence: true, numericality: true
+  validates :event_date, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_14_154319) do
+ActiveRecord::Schema.define(version: 2019_01_15_203759) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "artist"
+    t.text "description"
+    t.decimal "price_low"
+    t.decimal "price_high"
+    t.date "event_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "name"
