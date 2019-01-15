bccdf794a7b4de711060dacefd70dd06efa1cdfd
Wojciech Kumoń <wojtas626@gmail.com>
2019-01-15 21:59:59 +0100

    Events controller generated

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,14 @@
+class EventsController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/create'
+  get 'events/show'
   resources :tickets
   root to: 'tickets#index'
 end
