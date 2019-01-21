360cb634fe25ef038834774142e843699ee6c673
Wojciech Kumoń <wojtas626@gmail.com>
2019-01-21 22:04:10 +0100

    devise integrated

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap', '~> 4.2.1'
 gem 'jquery-rails'
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :events, :only => [:index, :new, :create, :show]
   # get 'events' => 'events#index'
   # get 'events/new'
