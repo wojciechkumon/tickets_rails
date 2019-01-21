bd36e0d08ec25cc16985af251af3f44b69ea7acf
Wojciech Kumoń <wojtas626@gmail.com>
2019-01-21 21:29:21 +0100

    Price and date validators for tickets and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,20 @@ class Event < ApplicationRecord
   has_many :tickets
 
   validates :artist, presence: true
-  validates :price_low, presence: true, numericality: true
-  validates :price_high, presence: true, numericality: true
+  validates :price_low, presence: true, numericality: {greater_than: 0}
+  validates :price_high, presence: true, numericality: {greater_than: 0}
   validates :event_date, presence: true
+  validate :event_date_not_from_past, :max_price_lower_than_min
 
+  def event_date_not_from_past
+    if event_date < Date.today
+      errors.add(:event_date, 'cannot be in past')
+    end
+  end
+
+  def max_price_lower_than_min
+    if price_high < price_low
+      errors.add(:price_high, 'cannot lower then low')
+    end
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,8 +2,17 @@ class Ticket < ApplicationRecord
 
   belongs_to :event
 
-  validates :name, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: {minimum: 6}
   validates :email_address, presence: true
   validates :price, presence: true
+  validate :valid_price
 
+  def valid_price
+    if price > event.price_high
+      errors.add(:price, "Price higher than allowed in this event (#{event.price_high})")
+    end
+    if price < event.price_low
+      errors.add(:price, "Price lower than allowed in this event (#{event.price_low})")
+    end
+  end
 end
