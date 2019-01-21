13faa90a119cfbcab3449b15869ce75f0fdf827b
Wojciech Kumoń <wojtas626@gmail.com>
2019-01-21 20:45:22 +0100

    Creating ticket for event

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -69,6 +69,6 @@ class TicketsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def ticket_params
-    params.require(:ticket).permit(:name, :seat_id_seq, :address, :price, :email_address, :phone)
+    params.require(:ticket).permit(:name, :seat_id_seq, :address, :price, :email_address, :phone, :event_id)
   end
 end
