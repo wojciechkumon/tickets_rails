c83714690b753393f6a9f1ef23f78f91b7e1b3e5
Wojciech Kumoń <wojtas626@gmail.com>
2019-01-21 19:57:42 +0100

    New main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'events' => 'events#index'
-  get 'events/new'
-  post 'events' => 'events#create'
-  get 'events/:id' => 'events#show'
+  resources :events, :only => [:index, :new, :create, :show]
+  # get 'events' => 'events#index'
+  # get 'events/new'
+  # post 'events' => 'events#create'
+  # get 'events/:id' => 'events#show'
   resources :tickets
-  root to: 'tickets#index'
 end
