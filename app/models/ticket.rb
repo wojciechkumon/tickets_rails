f01bf316cee2a0dcc3ca1b47f0013feb198ecebf
Wojciech Kumoń <wojtas626@gmail.com>
2019-01-21 22:24:49 +0100

    User ID added to ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 class Ticket < ApplicationRecord
 
   belongs_to :event
+  belongs_to :user
 
   validates :name, presence: true, length: {minimum: 6}
   validates :email_address, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_205740) do
+ActiveRecord::Schema.define(version: 2019_01_21_212221) do
 
   create_table "events", force: :cascade do |t|
     t.string "artist"
@@ -32,7 +32,9 @@ ActiveRecord::Schema.define(version: 2019_01_21_205740) do
     t.datetime "updated_at", null: false
     t.string "phone"
     t.integer "event_id"
+    t.integer "user_id"
     t.index ["event_id"], name: "index_tickets_on_event_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
